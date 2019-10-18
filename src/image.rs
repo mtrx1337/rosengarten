6905bd7f9ceaddd485bd3fc8e831b3a958220930
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-10-19 00:11:42 +0200

    added launch parameters, help function, cleaned up some stuff

--- src/image.rs
+++ src/image.rs
@@ -1,16 +1,17 @@
 use num::clamp;
+use std::string::String;
+use std::vec::Vec;
 
 pub struct Image {
-    pub width : u16,
-    pub height : u16,
-    pub gamma : f32,
-    pub filename : std::string::String,
-    pub image : std::vec::Vec<u8>,
+    pub width:    u16,
+    pub height:   u16,
+    pub gamma:    f32,
+    pub filename: String,
+    pub image:    Vec<u8>,
 }
 
-
 // array with chunks of 4 elements. r/g/b/a
-pub fn set_pixel(pixel_address : u64, mut color : [u8; 4], img : &mut Image) {
+pub fn set_pixel(pixel_address: u64, mut color: [u8; 4], img: &mut Image) {
     color = gamma_correct_color(color, img.gamma);
     // red
     img.image[pixel_address as usize]       = color[0];
@@ -22,11 +23,10 @@ pub fn set_pixel(pixel_address : u64, mut color : [u8; 4], img : &mut Image) {
     img.image[(pixel_address + 3) as usize] = color[3];
 }
 
-pub fn gamma_correct_color(mut color : [u8; 4], gamma : f32) -> [u8; 4] {
+pub fn gamma_correct_color(mut color: [u8; 4], gamma: f32) -> [u8; 4] {
     // value ^ (1/gamma)
-    color[0] = clamp(color[0].pow((1.0 as f32 / gamma) as u32), 0, 255);
-    color[1] = clamp(color[1].pow((1.0 as f32 / gamma) as u32), 0, 255);
-    color[2] = clamp(color[2].pow((1.0 as f32 / gamma) as u32), 0, 255);
-    color[3] = clamp(color[3].pow((1.0 as f32 / gamma) as u32), 0, 255);
+    for col in color.iter_mut() {
+        *col = clamp(col.pow((1.0 as f32 / gamma) as u32), 0, 255);
+    }
     return color;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
 extern crate num;
+
 mod ppm;
 mod image;
+
 use std::string::String;
+use std::vec::Vec;
 use num::clamp;
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+pub enum ParseArgsError {
+    ParseInt(ParseIntError),
+    NotEnoughArguments,
+}
+
+impl From<ParseIntError> for ParseArgsError {
+    fn from(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
+}
+
+pub fn parse_args() -> Result<(String, u16, u16), ParseArgsError> {
+    let argv = std::env::args().collect::<Vec<_>>();
+    match argv.get(1..4) {
+        Some(argv) => Ok((String::from(&argv[0]), argv[1].parse()?, argv[2].parse()?)),
+        None => Err(ParseArgsError::NotEnoughArguments)
+    }
+}
 
 fn main() {
-    let args : Vec<String> = std::env::args().collect();
-    let width : u16 = args[2].parse::<u16>().expect("Not enough arguments provided.");
-    let height : u16 = args[3].parse::<u16>().expect("Not enough arguments provided.");
-
-    let mut color : [u8; 4] = [0, 0, 0, 0];
-    let mut x : u16 = 0;
-    let mut y : u16 = 0;
-    let mut pixel_address : u64 = 0;
+    let (filename, width, height) = match parse_args() {
+        Ok(v) => v,
+        Err(e) => { println!("{:?}", e); print_help(); return; }
+    };
 
     let mut img = image::Image {
-        filename : String::from(&args[1]),
-        width    : width,
-        height   : height,
-        gamma    : 2.2,
-        image    : Vec::<u8>::new(),
+        filename: filename,
+        width: width,
+        height: height,
+        gamma: 2.2,
+        image: vec![0; (width * height * 4) as usize]
     };
+
+    let mut color: [u8; 4] = [0, 0, 0, 0];
+    let mut pixel_address: u64;
 
     for y in 0..height {
         for x in 0..width {
@@ -34,4 +57,10 @@ fn main() {
             image::set_pixel(pixel_address, color, &mut img);
         }
     }
+
+    ppm::write_rgb_ppm(&mut img);
+}
+
+fn print_help() {
+    println!("rgn <filename> <width> <height>");
 }
